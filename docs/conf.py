d483e2c7aad20c77ef258d64238ca84387ccb991
Sam Crawford <scrawfo6@icl.utk.edu>
2020-03-10 11:26:30 -0400

    no message

--- docs/conf.py
+++ docs/conf.py
@@ -30,14 +30,13 @@ release = '0.0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [''
-]
+extensions = ['sphinx.ext.todo']
 
 import sys, os
 
-sys.path.append(os.path.abspath('exts'))
+# sys.path.append(os.path.abspath('exts'))
 
-extensions = ['sphinxcontrib.bibtex']
+# extensions = ['sphinxcontrib.bibtex']
 
 
 # Add any paths that contain templates here, relative to this directory.
